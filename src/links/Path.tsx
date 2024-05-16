c0daef662f8e1a40a676e463b6968e9b84fff928
LilianBarbe <110779804+LilianBarbe@users.noreply.github.com>
2024-05-16 18:42:33 +0200

    new icon

--- src/links/Path.tsx
+++ src/links/Path.tsx
@@ -1,32 +1,32 @@
 import ILink from "./ILink";
-import { Action, ActionPanel, List } from "@raycast/api";
+import { Action, ActionPanel, List, Icon } from "@raycast/api";
 import { Link } from "../context";
 
 export default class Url implements ILink {
+  link: Link = null;
 
-    link: Link = null;
+  setLink(link: Link): ILink {
+    this.link = link;
+    return this;
+  }
 
-    setLink(link: Link): ILink {
-        this.link = link;
-        return this;
-    }
+  is = (link: string): boolean => {
+    return /^\//gm.test(link);
+  };
 
-    is = (link: string): boolean => {
-        return /^\//gm.test(link);
-    };
-
-    render(): any {
-        return (
-            <List.Item
-                key={this.link.link}
-                title={this.link.title}
-                subtitle={this.link.link} // Afficher uniquement le nom de domaine
-                actions={
-                    <ActionPanel>
-                        <Action.Open title={this.link.linkType} target={this.link.link} />
-                    </ActionPanel>
-                }
-            />
-        );
-    }
+  render(): any {
+    return (
+      <List.Item
+        key={this.link.link}
+        title={this.link.title}
+        subtitle={this.link.link} // Afficher uniquement le nom de domaine
+        icon={Icon.Folder}
+        actions={
+          <ActionPanel>
+            <Action.Open title={this.link.linkType} target={this.link.link} />
+          </ActionPanel>
+        }
+      />
+    );
+  }
 }
--- src/list-folders.tsx
+++ src/list-folders.tsx
@@ -1,112 +1,115 @@
 // list-folders.tsx
 import {
-    List,
-    ActionPanel,
-    Action,
-    Alert,
-    useNavigation,
-    confirmAlert,
-    showToast,
-    Toast,
-    popToRoot
+  List,
+  ActionPanel,
+  Action,
+  Alert,
+  useNavigation,
+  confirmAlert,
+  showToast,
+  Toast,
+  Icon,
+  popToRoot,
 } from "@raycast/api";
 import { FolderProvider, useFolders } from "./context";
 import { links } from "./links/LinkRegistar";
 
 // Assurez-vous que le `FolderProvider` enveloppe correctement `ProjectList`
 function ProjectList() {
-    const { folders, deleteFolder } = useFolders();
-    const { push } = useNavigation();
+  const { folders, deleteFolder } = useFolders();
+  const { push } = useNavigation();
 
-    async function handleDelete(folderName: string) {
-        const options = {
-            title: "Supprimer le dossier",
-            message: `Êtes-vous sûr de vouloir supprimer le dossier ${folderName} ?`,
-            primaryAction: {
-                onAction: () => {
-                    console.log("Action - Folder removed");
-                },
-                title: "Supprimer"
-            },
-            style: Action.Style.Destructive // Utiliser Action.Style.Destructive ici
-        } as Alert.Options;
+  async function handleDelete(folderName: string) {
+    const options = {
+      title: "Supprimer le dossier",
+      message: `Êtes-vous sûr de vouloir supprimer le dossier ${folderName} ?`,
+      primaryAction: {
+        onAction: () => {
+          console.log("Action - Folder removed");
+        },
+        title: "Supprimer",
+      },
+      style: Action.Style.Destructive, // Utiliser Action.Style.Destructive ici
+    } as Alert.Options;
 
-        const result = await confirmAlert(options);
-        if (result) {
-            deleteFolder(folderName);
-            showToast({ title: "Dossier supprimé", style: Toast.Style.Success });
-        }
+    const result = await confirmAlert(options);
+    if (result) {
+      deleteFolder(folderName);
+      showToast({ title: "Dossier supprimé", style: Toast.Style.Success });
     }
+  }
 
-    function handleProjectSelect(folderName: string) {
-        const selectedFolder = folders.find((folder) => folder.name === folderName);
-        if (selectedFolder) {
-            push(
-                <FolderProvider>
-                    <LinkList folder={selectedFolder} />
-                </FolderProvider>
-            );
-        }
+  function handleProjectSelect(folderName: string) {
+    const selectedFolder = folders.find((folder) => folder.name === folderName);
+    if (selectedFolder) {
+      push(
+        <FolderProvider>
+          <LinkList folder={selectedFolder} />
+        </FolderProvider>,
+      );
     }
+  }
 
-    return (
-        <List>
-            {folders.map((folder) => (
-                <List.Item
-                    key={folder.name}
-                    title={folder.name}
-                    actions={
-                        <ActionPanel>
-                            <Action title="Voir les liens" onAction={() => handleProjectSelect(folder.name)} />
-                            <Action
-                                title="Supprimer le dossier"
-                                style={Action.Style.Destructive}
-                                onAction={() => handleDelete(folder.name)}
-                            />
-                        </ActionPanel>
-                    }
-                />
-            ))}
-        </List>
-    );
+  return (
+    <List>
+      {folders.map((folder) => (
+        <List.Item
+          key={folder.name}
+          title={folder.name}
+          icon={Icon.Folder}
+          actions={
+            <ActionPanel>
+              <Action title="Voir les liens" onAction={() => handleProjectSelect(folder.name)} />
+              <Action
+                title="Supprimer le dossier"
+                style={Action.Style.Destructive}
+                onAction={() => handleDelete(folder.name)}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
 }
 
-function LinkList({ folder }: { folder: { name: string; links: { title: string; linkType: string, link: string; folder: string }[] } }) {
-    const { deleteLink } = useFolders();
+function LinkList({
+  folder,
+}: {
+  folder: { name: string; links: { title: string; linkType: string; link: string; folder: string }[] };
+}) {
+  const { deleteLink } = useFolders();
 
-    async function handleDelete(folderName: string, linkUrl: string) {
-        const options = {
-            title: "Supprimer le lien",
-            message: `Êtes-vous sûr de vouloir supprimer ce lien ?`,
-            primaryAction: {
-                onAction: () => {
-                },
-                title: "Supprimer"
-            },
-            style: Action.Style.Destructive // Utiliser Action.Style.Destructive ici aussi
-        };
+  async function handleDelete(folderName: string, linkUrl: string) {
+    const options = {
+      title: "Supprimer le lien",
+      message: `Êtes-vous sûr de vouloir supprimer ce lien ?`,
+      primaryAction: {
+        onAction: () => {},
+        title: "Supprimer",
+      },
+      style: Action.Style.Destructive, // Utiliser Action.Style.Destructive ici aussi
+    };
 
-        const result = await confirmAlert(options);
-        if (result) {
-            deleteLink(folderName, linkUrl);
-            showToast({ title: "Lien supprimé", style: Toast.Style.Success });
-            popToRoot(); // Retourner à l'écran précédent après la suppression
-        }
+    const result = await confirmAlert(options);
+    if (result) {
+      deleteLink(folderName, linkUrl);
+      showToast({ title: "Lien supprimé", style: Toast.Style.Success });
+      popToRoot(); // Retourner à l'écran précédent après la suppression
     }
+  }
 
-    return (
-        <List navigationTitle={`Liens dans le projet ${folder.name}`}>
-            {folder.links.map((link) => (
-                links[link.linkType].setLink(link).render()
-            ))}
-        </List>
-    );
+  return (
+    <List navigationTitle={`Liens dans le projet ${folder.name}`}>
+      {folder.links.map((link) => links[link.linkType].setLink(link).render())}
+    </List>
+  );
 }
 
 export default function ListFoldersCommand() {
-    return (
-        <FolderProvider>
-            <ProjectList />
-        </FolderProvider>
-    );
+  return (
+    <FolderProvider>
+      <ProjectList />
+    </FolderProvider>
+  );
 }
